ba58b613b602a130f13a384887c1f2ec0470a439
Arjun-G-Ravi <arjungravi1@gmail.com>
2023-09-23 20:35:20 +0530

    Added log reg

--- ml_algos.py
+++ ml_algos.py
@@ -1,14 +1,10 @@
 import regression
 import speech
 
-class mainClass(): # just a sample, mainly for docs
+class main(): # just a sample, mainly for docs
     def __init__(self):
         print("Thank you for using ml_algos")
 
     def docs(self):
-        print( """The current version contains the following implemented algorithms:
-            1. Linear Regression # fit, predict, score
-            2. Logistic Regression
-              
-              """)
+        print( 'The docs are inaccessible right now.')
         
--- regression.py
+++ regression.py
@@ -38,12 +38,56 @@ class LinearRegression:
             plot([i for i in range(1,len(self.loss_per_epoch)+1)],self.loss_per_epoch,color='r',marker='.',)
         
     def predict(self, X):
-        return [self.predict_one_(x) for x in X]
+        return [self._predict_one(x) for x in X]
         
     def score(self, X, y):
         # last mse error
         return self.loss_per_epoch[-1]
 
-    def predict_one_(self, x):
+    def _predict_one(self, x):
         # f(x) = w*x + b
         return np.dot(self.weights,x) + self.bias
+    
+
+class LogisticRegression:
+    '''
+    Parameters: lr, num_epochs, show_cost, show_cost_graph
+    Methods: fit, predict, score
+
+    '''
+    def __init__(self, lr=0.01, num_epochs=1000): 
+        self.lr = lr
+        self.num_epochs = num_epochs
+        self.weights = None
+        self.bias = None
+
+    def fit(self,X,y):
+        m, n = X.shape # m is the number of training data and n is the number of features
+        self.weights = np.zeros(n)
+        self.bias = 0
+
+        for epoch in range(self.num_epochs):
+            y_pred = self.predict(X)
+
+            dJ_dw = (1/m)*np.dot(y_pred-y,X)
+            dJ_db = (1/m)*np.sum(y_pred-y)
+
+            self.weights -= self.lr * dJ_dw
+            self.bias -= self.lr * dJ_db
+
+        
+    def predict(self, X):
+        out1 = [self._predict_one(x) for x in X]
+        out = [1 if i>0.5 else 0 for i in out1]
+        return out
+    
+        
+    def score(self, X, y):
+        return (1/len(y))*np.sum(self.predict(X) == y)
+
+    def _predict_one(self, x):
+        return self._sigmoid(np.dot(self.weights,x) + self.bias) # f(x) = sigmoid(w*x + b)
+    
+    def _sigmoid(self, z):
+        z = np.clip(z, -500, 500)
+        return (1/(1 + np.exp(-z)))
