3500cdd318d455bf8b19cf70584ff4e48b50cda1
Arjun-G-Ravi <arjungravi1@gmail.com>
2024-05-19 07:03:54 +0530

    Added path correction to one test case

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
-from skmini.classification.classification import LogisticRegression
+import set_path
 
+from skmini.classification import LogisticRegression
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 ds = load_breast_cancer()
