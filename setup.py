a9ba3f89ca7e050479c969e1938faa858e088d57
Arjun-G-Ravi <arjungravi1@gmail.com>
2024-07-01 20:43:51 +0530

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
-
+# This lets the repo to be installed using 'pip install -e .' at root
 setup(
-    name='my_package',
+    name='skmini',
     version='0.1',
     packages=find_packages(),
     install_requires=[
