f19305cdfd4e0fff85606b4f1c0bbb4a53549f59
Arjun-G-Ravi <arjungravi1@gmail.com>
2024-07-10 13:28:29 +0530

    Logistic regression works, but is erratic at start. Still couldnt fix

--- tests/unit_tests/test_logistic_regression.py
+++ tests/unit_tests/test_logistic_regression.py
@@ -10,7 +10,7 @@ class TestLogisticRegression:
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             ds1["data"], ds1["target"]
         )
-        model = LogisticRegression(verbose=True, num_epochs=1000)
+        model = LogisticRegression(verbose=True, num_epochs=100)
 
         model.fit(self.X_train, self.y_train)
         train_score = model.score(self.X_train, self.y_train)
@@ -22,7 +22,7 @@ class TestLogisticRegression:
     def test_train_custom_data(self):
         X = np.array([[3], [5], [7], [9], [12]])
         y = np.array([0, 0, 1, 1, 1])
-        model = LogisticRegression(num_epochs=10000,verbose=True )
+        model = LogisticRegression(num_epochs=100,verbose=True )
         model.fit(X, y)
         train_score = model.score(X,y)
         prediction = model.predict(X)
@@ -32,6 +32,7 @@ class TestLogisticRegression:
 
 if __name__ == "__main__":
     f = TestLogisticRegression()
-    # f.test_train_model_breastcancer()
+    f.test_train_model_breastcancer()
     # print(' - '*40)
-    f.test_train_custom_data()
+    # f.test_train_custom_data()
+
