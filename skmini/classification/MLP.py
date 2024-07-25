9655c0ea6ae08326506df9c0787086943a78cdcc
Arjun-G-Ravi <arjungravi1@gmail.com>
2024-07-25 22:46:36 +0530

    Committed

--- skmini/classification/MLP.py
+++ skmini/classification/MLP.py
@@ -56,7 +56,6 @@ class Value:
             if child.op:
                 child.backprop()
 
-
 class Neuron:
     def __init__(self):
         self.w = None
@@ -104,19 +103,6 @@ class MLP:
 
 # test -> will change to tests in the future
 if __name__ == "__main__":
-    # from sklearn.model_selection import train_test_split
-    # from sklearn import datasets
-
-    # X, y = datasets.make_classification(
-    #     n_samples=100, n_features=2, n_classes=2, n_redundant=0, random_state=42
-    # )
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #     X, y, test_size=0.2, random_state=123
-    # )
-    # print("X.shape:", X.shape)
-    # obj = MLPClassifier()
-    # obj.fit(X_train, y_train)
-
     a = Value(2)
     b = Value(4)
     c = a + b
