537bb6ba88c9f463bf7d6f69e0eb159c705f6fed
arjungravi1@gmail.com <arjungravi1@gmail.com>
2024-11-17 06:05:27 +0530

    completed KNN

--- skmini/classification/KNN.py
+++ skmini/classification/KNN.py
@@ -4,6 +4,8 @@ import numpy as np
 class KNN:
     def __init__(self, k=5):
         self.k = k
+        self.X = None
+        self.y = None
 
     def fit(self, X, y):
         self.X = X
@@ -11,12 +13,12 @@ class KNN:
 
     def _predict_one(self, x):
         neighbours = []
-        assert x.shape == self.X[0].shape
+        # if self.y == None: raise 'Error: Training not performed'
+        assert x.shape == self.X[0].shape, 'Error: Mismatch in training and prediction shape'
         for x_, y_ in zip(self.X, self.y):
             neighbours.append((self._calc_distance(x, x_), y_))
         neighbours.sort()
         top_k_nearest_neighbours = neighbours[: self.k] # top-k neighbours
-        score = 0
         y_classes = set(self.y)
         data = {k:0 for k in y_classes}
         for i in top_k_nearest_neighbours:
@@ -25,6 +27,19 @@ class KNN:
 
     def _calc_distance(self, x1, x2):
         return (np.sum((x1 - x2)**2))**0.5
+    
+    def score(self, Xtest, ytest):
+        _score = 0
+        for x_, y_ in zip(Xtest, ytest):
+            _score += self._predict_one(x_) == y_
+        return _score/len(ytest)
+    
+    def predict(self, x):
+        out = []
+        for i in x:
+            out.append(self._predict_one(i))
+        return np.array(out)
+
 
 
 if __name__ == "__main__":
