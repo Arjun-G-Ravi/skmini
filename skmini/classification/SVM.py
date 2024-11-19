d44b16fcd4c7a6a0eb284724d9c6ac95155c6798
arjungravi1@gmail.com <arjungravi1@gmail.com>
2024-11-20 05:17:40 +0530

    completed basic SVM implementation

--- skmini/classification/SVM.py
+++ skmini/classification/SVM.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 class SVC:
@@ -7,43 +6,26 @@ class SVC:
         self.kernel = kernel
         assert self.kernel == 'linear', 'Only linear SVM implemented'
 
-    def fit(self, X_train, y_train, n_epochs=10, lr=0.01, lambda_param = 0.01):
+    def fit(self, X_train, y_train, epochs=10, lr=0.01, lambda_param = 0.01):
         self.n, self.m = X_train.shape # n is num_samples, m is num_features
         self.w = np.array([np.random.randn(1) for i in range(self.m)]).reshape(-1) * 0.01
         self.b = np.random.randn(1) * 0.01
         self.lr = lr
         self.lambda_param = lambda_param
-        total_loss = 0
         y_ = np.where(y_train <= 0, -1, 1)
 
-        for i in range(n_epochs):
+        for i in range(epochs):
+            epoch_loss = 0
             for id in range(self.n):
                 score = self.w@X_train[id] + self.b
+                # Calculate hinge loss for monitoring
+                loss = max(0, 1 - y_[id] * score[0]) 
+                epoch_loss += loss
                 if (y_[id]* score) >=1:
                     # Correct classification with margin
                     self.w -= self.lr * (2 * self.lambda_param * self.w )
                 else:
                     # Incorrect classification or within margin
-                    # print((2 * self.lambda_param * self.w).shape, X_train[id].shape, y_[id].shape, )
                     self.w -= self.lr * (2 * self.lambda_param * self.w  - X_train[id]*y_[id])
                     self.b -= self.lr * y_[id]
-                # Calculate hinge loss for monitoring
-                # print(y_[id], score)
-                loss = max(0, 1 - y_[id] * score[0]) 
-                print(loss)
-
-
-        
-
-# if __name__ == '__main__':
-#     from skmini.datasets import load_iris
-#     # X = load_iris()['data']
-#     # y = load_iris()['target']
-#     # print(X.shape, y.shape)
-
-#     X = [3,4,5]
-#     y = [3,4,5]
-#     plt.plot(X, y)
-#     plt.show()
-#     svm = SVC()
-#     svm.fit(X, y)
+            print(epoch_loss)
