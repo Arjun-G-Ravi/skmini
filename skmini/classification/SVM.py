943d755b967f0e8baee7060eef2727772a0ccff4
arjungravi1@gmail.com <arjungravi1@gmail.com>
2024-11-19 07:28:39 +0530

    didnt fully fix

--- skmini/classification/SVM.py
+++ skmini/classification/SVM.py
@@ -5,23 +5,33 @@ import matplotlib.pyplot as plt
 class SVC:
     def __init__(self, kernel='linear'):
         self.kernel = kernel
+        assert self.kernel == 'linear', 'Only linear SVM implemented'
 
-    def fit(self, X_train, y_train):
-        self.dim = X_train.shape[-1] - 1
-        self.w = [np.random.randn(1) for i in range(self.dim-1)]
-        self.b = np.random.randn(1)
-        
+    def fit(self, X_train, y_train, n_epochs=10, lr=0.01, lambda_param = 0.01):
+        self.n, self.m = X_train.shape # n is num_samples, m is num_features
+        self.w = np.array([np.random.randn(1) for i in range(self.m)]).reshape(-1) * 0.01
+        self.b = np.random.randn(1) * 0.01
+        self.lr = lr
+        self.lambda_param = lambda_param
+        total_loss = 0
+        y_ = np.where(y_train <= 0, -1, 1)
 
-        print(self.w)
-        print(X_train)
-        y = np.arange(-10, 11, 10)
-        print(y)
+        for i in range(n_epochs):
+            for id in range(self.n):
+                score = self.w@X_train[id] + self.b
+                if (y_[id]* score) >=1:
+                    # Correct classification with margin
+                    self.w -= self.lr * (2 * self.lambda_param * self.w )
+                else:
+                    # Incorrect classification or within margin
+                    # print((2 * self.lambda_param * self.w).shape, X_train[id].shape, y_[id].shape, )
+                    self.w -= self.lr * (2 * self.lambda_param * self.w  - X_train[id]*y_[id])
+                    self.b -= self.lr * y_[id]
+                # Calculate hinge loss for monitoring
+                # print(y_[id], score)
+                loss = max(0, 1 - y_[id] * score[0]) 
+                print(loss)
 
-        plt.plot((X_train*self.w).reshape(3,), y)
-        plt.scatter(X_train,y_train)
-    
-    def _distance(self, x):
-        return 1
 
         
 
